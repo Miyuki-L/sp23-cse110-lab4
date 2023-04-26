ae6c9c624f29cce4bec87971bcfdae9494566a23
Helen <59126064+Miyuki-L@users.noreply.github.com>
2023-04-26 13:10:52 -0700

    Completed Expose Part 1

---
+++ expose/javascript/ProvidedCode/sumValues.js
@@ -0,0 +1,16 @@
+function sumValues(num1, num2, add) {
+   if (add) {
+      // var result = 0; // question 1,2
+      // let result = 0; // question 3,4
+      const result = 0;
+
+      result = num1+num2;
+
+      console.log('values added: ', result);
+
+   } else return;
+
+   console.log('final result: ', result);
+}
+
+sumValues(10,10, true);
